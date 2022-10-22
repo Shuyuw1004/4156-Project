96f008f20db2b6a646a0f1a02134860fde4ac962
Wannuo Sun <ws2591@columbia.edu>
2022-10-21 21:47:22 -0400

    Temp code for setting oauth2 for API authorization

--- rentermatch/src/main/java/com/humanlearning/rentermatch/config/AuthServerConfiguration.java
+++ rentermatch/src/main/java/com/humanlearning/rentermatch/config/AuthServerConfiguration.java
@@ -1,4 +1,51 @@
 package com.humanlearning.rentermatch.config;
 
-public class AuthServerConfiguration {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${user.oauth2.clientId}")
+    private String clientID;
+
+    @Value("${user.oauth.clientSecret}")
+    private CharSequence clientSecret;
+
+    @Value("${user.oauth.redirectUris}")
+    private String redirectURLs;
+
+    @Value("${user.oauth.accessTokenValidity}")
+    private int accessTokenValidity;
+
+    @Value("${user.oauth.refreshTokenValidity}")
+    private int refreshTokenValidity;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+        oauthServer.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    public void configure(ClientDetailsServiceConfigurer clients) {
+        clients
+                .inMemory()
+                .withClient(clientID)
+                .secret(passwordEncoder.encode(clientSecret))
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                .scopes("user_info")
+                .authorities("")
+                .redirectUris(redirectURLs)
+                .accessTokenValiditySeconds(accessTokenValidity)
+                .refreshTokenValiditySeconds(refreshTokenValidity);
+    }
 }
