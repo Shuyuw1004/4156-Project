98351f5954a7cafd396daba2ddbe7b7746250b2a
Yan_Ye <evelynye0102@gmail.com>
2022-12-17 13:10:23 -0500

    landlord user-home finished without table

--- Client_UI_New/src/app/landlord-user-home/landlord-user-home.component.ts
+++ Client_UI_New/src/app/landlord-user-home/landlord-user-home.component.ts
@@ -11,7 +11,8 @@ export class LandlordUserHomeComponent implements OnInit {
   name :string = "";
   email : string = "";
   clientId : string = '';
-  getMatchResponse : string = "";
+  getZipcodeResponse : string = "";
+  getTenantsByZipcode : string = "";
 
   constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
@@ -41,7 +42,31 @@ export class LandlordUserHomeComponent implements OnInit {
   }
 
   getZipcode() {
-    let url = "http://127.0.0.1:8080/landlord/getZipcode";
+    let url = "http://127.0.0.1:8080/client/getZipcode";
+    let url2 = "http://127.0.0.1:8080/client/getTenantByZipcode";
+    this.httpClient.get(url,{observe: 'body', responseType: "text"}).subscribe({
+      next: next => {
+        console.log(next);
+        this.getZipcodeResponse = next.toString();
+        let params = new HttpParams()
+          .append("zipcode", this.getZipcodeResponse);
+        this.httpClient.get(url2, {params: params, observe: 'body', responseType: "text"}).subscribe(
+          {
+            next: next => {
+            this.getTenantsByZipcode = next;
+            },
+            error: error => {
+              console.log(error);
+              this.getTenantsByZipcode = error.error;
+            }
+          }
+        );
+      },
+      error: error => {
+        console.log(error);
+        this.getZipcodeResponse = error.error;
+      }
+    });
 
   }
 
