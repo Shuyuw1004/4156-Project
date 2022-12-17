3f48b220bfc1cb4ba564d5a0e3d1aad6d2b3c546
Yan_Ye <evelynye0102@gmail.com>
2022-12-17 17:15:31 -0500

    realtor user-home finished

--- Client_UI_New/src/app/realtor-user-home/realtor-user-home.component.ts
+++ Client_UI_New/src/app/realtor-user-home/realtor-user-home.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -9,13 +9,54 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class RealtorUserHomeComponent implements OnInit {
 
+  name : string = "";
+  email : string = "";
+  clientId : string = "";
+  getTenantsResponse : string = "";
   constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParamMap
+      .subscribe((params) => {
+          let temp_name = params.get('name');
+          if ( temp_name !== null){
+            this.name = temp_name;
+          } else {
+            this.name = "";
+          }
+          let temp_email = params.get('email');
+          if ( temp_email !== null){
+            this.email = temp_email;
+          } else {
+            this.email = "";
+          }
+          let temp_clientId = params.get('clientId');
+          if ( temp_clientId !== null){
+            this.clientId = temp_clientId;
+          } else {
+            this.clientId = "";
+          }
+        }
+      );
   }
 
   getTenants() {
-    let url = "http://127.0.0.1:8080/realtor/getTenants";
+    let url = "http://127.0.0.1:8080/client/getTenantByZipcode";
+    let zipcode = (<HTMLInputElement>document.querySelector('[name="zipcode"]')).value;
+    let params = new HttpParams()
+      .append("zipcode", zipcode);
+    this.httpClient.get(url,{params: params, observe: 'body', responseType: "text"}).subscribe({
+      next: next => {
+        console.log(next);
+        this.getTenantsResponse = next.toString();
+      },
+      error: error => {
+        console.log(error);
+        this.getTenantsResponse = error.error;
+      }
+    });
+    return false;
+
 
   }
 
