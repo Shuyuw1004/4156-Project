254b962b54c2649a37b3d1d4c96a340fb844ca55
Yan_Ye <evelynye0102@gmail.com>
2022-12-17 02:35:23 -0500

    change frontend pages

--- Client_UI_New/src/app/app-routing.module.ts
+++ Client_UI_New/src/app/app-routing.module.ts
@@ -1,23 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LandlordRegisterComponent } from './landlord-register/landlord-register.component';
-import { RegisterComponent } from './register/register.component';
-import { SigninComponent } from './signin/signin.component';
 import { TenantRegisterComponent } from './tenant-register/tenant-register.component';
 import { TenantUpdateComponent } from './tenant-update/tenant-update.component';
 import { UserHomeComponent} from "./user-home/user-home.component";
+import {RealtorRegisterComponent} from "./realtor-register/realtor-register.component";
+import {RealtorSigninComponent} from "./realtor-signin/realtor-signin.component";
+import {RealtorUserHomeComponent} from "./realtor-user-home/realtor-user-home.component";
+import {LandlordSigninComponent} from "./landlord-signin/landlord-signin.component";
+import {LandlordUserHomeComponent} from "./landlord-user-home/landlord-user-home.component";
+import {SigninComponent} from "./signin/signin.component";
+import {RegisterComponent} from "./register/register.component";
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {LandlordRegisterComponent} from "./landlord-register/landlord-register.component";
 
 const routes: Routes = [
   { component: RegisterComponent, path: "" },
   { component: LandlordRegisterComponent, path: "landlordRegister" },
+  { component: LandlordSigninComponent, path: "landlordSignin"},
+  { component: LandlordUserHomeComponent, path: "landlordUserHome"},
   { component: TenantRegisterComponent, path: "tenantRegister" },
+  { component: RealtorRegisterComponent, path: "realtorRegister"},
+  { component: RealtorSigninComponent, path: "realtorSignin"},
+  { component: RealtorUserHomeComponent, path: "realtorUserHome"},
   { component: SigninComponent, path: "signin"},
   { component: UserHomeComponent, path: "userHome"},
   { component: TenantUpdateComponent, path: "tenantUpdate"}
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
