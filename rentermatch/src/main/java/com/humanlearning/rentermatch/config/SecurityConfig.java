ff8ad0c7b0e15d07c2c7ec0f48519d41356bade5
Wannuo Sun <ws2591@columbia.edu>
2022-12-18 09:01:34 +0800

    Add csrf

--- rentermatch/src/main/java/com/humanlearning/rentermatch/config/SecurityConfig.java
+++ rentermatch/src/main/java/com/humanlearning/rentermatch/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
+    http.csrf()
+        .disable()
         .authorizeRequests()
         .antMatchers("/authenticate")
         .permitAll()
