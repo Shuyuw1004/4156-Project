b78a672d91298d58c0a0f11bdd1d5561399b3d3e
Wannuo Sun <ws2591@columbia.edu>
2022-12-18 08:53:02 +0800

    Fix security problem

--- rentermatch/src/main/java/com/humanlearning/rentermatch/config/SecurityConfig.java
+++ rentermatch/src/main/java/com/humanlearning/rentermatch/config/SecurityConfig.java
@@ -38,8 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf()
-        .disable()
+    http
         .authorizeRequests()
         .antMatchers("/authenticate")
         .permitAll()
